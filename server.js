5c367e41741262a7e1ad74f03691c03c5dc519b5
budou114 <58349030+budou114@users.noreply.github.com>
2024-07-23 15:29:57 +0900

    16. ユーザーログインAPIを作成してみよう

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 // 新規ユーザー登録API
 app.post("/api/auth/register", async (req, res) => {
     const { username, email, password } = req.body;
-    console.log(username, email, password);
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -25,6 +24,23 @@ app.post("/api/auth/register", async (req, res) => {
     });
 
     return res.json({ user });
+});
+
+// ユーザーログインAPI
+app.post("/api/auth/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = prisma.user.findUnique({ where: { email }});
+
+    if (!user) {
+        return res.status(401).json({ error: "そのユーザーは存在しません。" });
+    }
+
+    const isPasswordVaild = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordVaild) {
+        return res.status(401).json({ error: "そのパスワードは間違っています。" });
+    }
 });
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));

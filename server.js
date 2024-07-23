b50f60e05581c01b8b98a73be6c6e13b3a8b2206
budou114 <58349030+budou114@users.noreply.github.com>
2024-07-23 14:58:53 +0900

    13. ユーザー新規登録APIを作成してみよう

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client");
 
 const PORT = 5050;
+
+const prisma = new PrismaClient();
+
+// 新規ユーザー登録API
+app.post("/api/auth/register", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password,
+        },
+    });
+
+    return res.json({ user });
+});
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));

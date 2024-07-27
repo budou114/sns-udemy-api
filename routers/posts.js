4cfe1c0db659737d7c3e330754a2c10081433a30
budou114 <58349030+budou114@users.noreply.github.com>
2024-07-27 12:29:05 +0900

    35. 最新投稿を10件取得するAPIを作ってみよう

--- routers/posts.js
+++ routers/posts.js
@@ -28,26 +28,17 @@ router.post("/post", async (req, res) => {
 });
 
 // 最新呟き取得用API
-// router.post("/login", async (req, res) => {
-//     const { email, password } = req.body;
-
-//     const user = await prisma.user.findUnique({ where: { email }});
-
-//     if (!user) {
-//         return res.status(401).json({ error: "そのユーザーは存在しません。" });
-//     }
-
-//     const isPasswordVaild = await bcrypt.compare(password, user.password);
-
-//     if (!isPasswordVaild) {
-//         return res.status(401).json({ error: "そのパスワードは間違っています。" });
-//     }
-
-//     const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-//         expiresIn: "1d",
-//     });
-
-//     return res.json({ token });
-// });
+router.get("/get_latest_post", async (req, res) => {
+    try {
+        const latestPosts = await prisma.post.findMany({
+            take: 10,
+            orderBy: { createdAt: "desc" },
+        });
+        return res.json(latestPosts);
+    } catch {
+        console.log(error);
+        res.status(500).json({ message: "サーバーエラーです。" });
+    }
+});
 
 module.exports = router;

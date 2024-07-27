957efa937915d1c9cb1406f6b24b5c51013de3ba
budou114 <58349030+budou114@users.noreply.github.com>
2024-07-27 16:28:42 +0900

    43. ログインユーザー取得用APIを構築してみよう

---
+++ routers/user.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+const prisma = new PrismaClient();
+
+router.get("/find", isAuthenticated, async (req, res) => {
+    try {
+        const user = await prisma.findUnique({
+            where: {
+                id: req.userId,
+            },
+        });
+
+        if (!user) {
+            res.status(404).json({ error: "ユーザーが見つかりませんでした。" });
+        }
+
+        res.status(200).json({
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username
+            },
+        });
+    } catch {
+        console.log(error);
+        res.status(500).json({ message: "サーバーエラーです。" });
+    }
+});
+
+module.exports = router;

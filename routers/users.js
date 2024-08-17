0aa7169610899f372ee4bd51008f2a477b61ae18
budou114 <58349030+budou114@users.noreply.github.com>
2024-08-17 12:14:37 +0900

    54. プロフィール取得用APIを構築してみよう

--- routers/users.js
+++ routers/users.js
@@ -25,7 +25,35 @@ router.get("/find", isAuthenticated, async (req, res) => {
         });
     } catch(error) {
         console.log(error);
-        res.status(500).json({ message: "サーバーエラーです。" });
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/profile/:userId", async(req, res) => {
+    const {userId} = req.params;
+
+    try {
+        const profile = await prisma.profile.findUnique({
+            where: { userId: parseInt(userId) },
+            include: {
+                user: {
+                    include: {
+                        profile: true,
+                    }
+                }
+            }
+        });
+
+        if (!profile) {
+            return res
+                .status(404)
+                .json({ message: "プロフィールが見つかりませんでした。"});
+        }
+
+        res.status(200).json(profile);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
     }
 });
 

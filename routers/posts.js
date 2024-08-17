ecce2ce4ef5e9c53e847c262fd8944610c6abc6c
budou114 <58349030+budou114@users.noreply.github.com>
2024-08-17 10:46:57 +0900

    52. アイコン画像が表示されないバグを修正してみよう

--- routers/posts.js
+++ routers/posts.js
@@ -19,7 +19,11 @@ router.post("/post", isAuthenticated, async (req, res) => {
                 authorId: req.userId,
             },
             include: {
-                author: true,
+                author: {
+                    include: {
+                        profile: true,
+                    }
+                }
             },
         });
 
@@ -38,7 +42,11 @@ router.get("/get_latest_post", async (req, res) => {
             take: 10,
             orderBy: { createdAt: "desc" },
             include: {
-                author: true,
+                author: {
+                    include: {
+                        profile: true,
+                    }
+                }
             },
         });
         return res.json(latestPosts);
